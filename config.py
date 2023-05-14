6cb29c6990721bb3927d2c8627d4e9609f4e9954
Mig <r121221@yandex.ru>
2023-05-14 12:04:16 +0300

    соединение БД через app end

--- config.py
+++ config.py
@@ -3,18 +3,21 @@ from environs import Env
 
 
 class Config:
-    SECRET_KEY =  os.environ.get('SECRET KEY') or'dhme;kghjanrkael/jgbuilarelkjmgbipuehjmghiotrkjhtoikle'
+    SECRET_KEY = os.environ.get('SECRET KEY') or 'dhme;kghjanrkael/jgbuilarelkjmgbipuehjmghiotrkjhtoikle'
     DEBUG = True
     DATABASE = 'bd_vk_post.db'
 
+
 def load_VK(path: str | None = None) -> Config:
     env = Env()
-    env.read_env(os.path.join(path,'.env'))
-    print('7777',os.path.join(path,'.env'))
-    print('8888',env('VK_TOKEN'))
+    env.read_env(os.path.join(path, '.env'))
+    print('7777', os.path.join(path, '.env'))
+    print('8888', env('VK_TOKEN'))
 
     return env('VK_TOKEN')
+
+
 def load_BOT(path: str | None = None) -> Config:
     env = Env()
-    env.read_env(os.path.join(path,'.env'))
+    env.read_env(os.path.join(path, '.env'))
     return env('BOT_TOKEN')
